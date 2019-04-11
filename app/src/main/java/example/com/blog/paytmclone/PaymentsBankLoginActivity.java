77c904958a09bd9bb1cdd715cd4273213dcdd82a
Priyanshu Shekhar <priyanshushekhar.777@gmail.com>
2019-04-11 16:23:10 +0530

    Initial commit

---
+++ app/src/main/java/example/com/blog/paytmclone/PaymentsBankLoginActivity.java
@@ -0,0 +1,50 @@
+package example.com.blog.paytmclone;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class PaymentsBankLoginActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_mall:
+                    mTextMessage.setText(R.string.title_mall);
+                    return true;
+                case R.id.navigation_scan:
+                    mTextMessage.setText(R.string.title_scan);
+                    return true;
+                case R.id.navigation_bank:
+                    mTextMessage.setText(R.string.title_bank);
+                    return true;
+                case R.id.navigation_inbox:
+                    mTextMessage.setText(R.string.title_inbox);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payments_bank_login);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
